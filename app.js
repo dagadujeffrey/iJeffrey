7b75d82d7ddcbb9d87e805cd73d311336d2a2616
Dagadu <DagaduJ@stanbic.com.gh>
2017-09-21 15:07:43 +0000

    Created app.js and package.json

---
+++ app.js
@@ -0,0 +1,24 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+//Creat Chat Bot
+var connector = new builder.ChatConnector({
+    appId: '07690c06-c464-4b5e-8ae6-314e477b1b6f',
+    appPassword: 'KT1Ms2nYkHDR6fAW5bqMTcW'
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
+
